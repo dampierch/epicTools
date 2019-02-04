2c0016d05cf5a5d0a26f5c2c47173ff22580d851
dampierch <odampso@yahoo.com>
2019-02-04 16:40:57 -0500

    rough diet order for group

--- npoOrder.R
+++ npoOrder.R
@@ -1,71 +1,44 @@
 #####
-##### Test code for proposed diet order
+##### Proof of concept code for proposed diet order
 #####
 
-#to do: 1) summarize diet order 2) fix input so only integers accepted
+
 ### goals of this workflow
 
-  # every patient gets oral diet order option
-  # only patients with enteral access get enteral diet order option
+  # every inpatient gets oral diet order option
   # only patients with oral diet order != regular get clinical condition
   # only patients with oral diet == "nothing w/ exceptions" get exceptions option
+  # only patients with enteral access get enteral diet order option
   # only patients with enteral diet order option !=NA and !="nothing at all" get proximal limit option
 
 
 ### set options menus
 
 oral_diet_options <- c("regular", "therapeutic", "nothing at all", "nothing with exceptions")
-enteral_diet_options <- c("tube feeds", "meds", "flushes", "nothing at all")
 clinical_condition_options <- c("awaiting surgery/procedure", "aspiration risk/awaiting speech eval", "dysphagia", "ileus/obstruction", "ischemia/inflammation", "other")
-oral_diet_exceptions <- c("meds", "sips with meds", "ice chips", "gum")
+npo_exceptions_options <- c("meds", "sips with meds", "ice chips", "gum")
+enteral_diet_options <- c("tube feeds", "meds", "flushes", "nothing at all")
 enteral_access_options <- c("gastric", "post-pyloric", "jejunal")
 
 
 ### set EMR data
 
 possible_ldas <- c("NGT", "OGT", "PEG" ,"PEG-J", "J", "DHT", "CVL", "art", "PIV", "ETT")
-enteral_ldas <- c("NGT", "OGT", "PEG" ,"PEG-J", "J", "DHT")
+enteral_ldas <- c("NGT", "OGT", "PEG" ,"PEG-J", "J", "DHT", "NJT")
+jejunal_ldas <- c("PEG-J", "J", "NJT")
+gastric_ldas <- c("NGT", "OGT", "PEG", "DHT")
 
 
 ### set variables
 
 odi <- NULL
-edi <- NULL
 cci <- NULL
 nei <- NULL
+edi <- NULL
 eai <- NULL
 
 
 ### define functions
-
-#get oral diet preference
-oral_diet_idx <- function()
-{
-  print.noquote("What can the patient swallow by mouth?")
-  print.noquote("Oral diet order options:")
-  for (i in 1:length(oral_diet_options)) {
-    print.noquote(paste(i, oral_diet_options[i], sep = " - "))
-  }
-  inp <- readline(prompt="Enter oral diet order as integer: ")
-  return(as.integer(inp))
-}
-
-#get enteral diet preference
-enteral_diet_idx <- function()
-{
-  print.noquote("What can the patient be given by tube?")
-  print.noquote("Enteral diet order options:")
-  for (i in 1:length(enteral_diet_options)) {
-    print.noquote(paste(i, enteral_diet_options[i], sep = " - "))
-  }
-  inp <- NA
-  while (sum(grepl("done", inp)) < 1) {
-    inp <- append(inp, readline(prompt="Enter enteral diet order as integer (type done when finished): "))
-  }
-  inp <- inp[!is.na(inp)]
-  inp <- inp[c(1:length(inp)-1)]
-  return(as.vector(inp))
-}
 
 #get active ldas (should not require user input)
 active_ldas_idx <- function()
@@ -76,15 +49,38 @@ active_ldas_idx <- function()
     print.noquote(paste(i, possible_ldas[i], sep = " - "))
   }
   inp <- NA
-  while (sum(grepl("done", inp)) < 1) {
-    inp <- append(inp, readline(prompt="Enter active LDAs as integer (type done when finished): "))
+  while (sum(grepl(0, inp)) < 1) {
+    new <- readline(prompt="Enter active LDAs as integer (type 0 when done): ")
+    new <- as.integer(new)
+    if (is.na(new)) {
+      print.noquote("*** Entry must be INTEGER ***")
+      new <- active_ldas_idx()
+    }
+    inp <- append(inp, new)
   }
   inp <- inp[!is.na(inp)]
   inp <- inp[c(1:length(inp)-1)]
   return(as.vector(inp))
 }
 
-#get clinical condition
+#get oral diet preference (every inpatient)
+oral_diet_idx <- function()
+{
+  print.noquote("What can the patient be given by mouth?")
+  print.noquote("Oral diet order options:")
+  for (i in 1:length(oral_diet_options)) {
+    print.noquote(paste(i, oral_diet_options[i], sep = " - "))
+  }
+  inp <- readline(prompt="Enter oral diet order as integer: ")
+  inp <- as.integer(inp)
+  if (is.na(inp)) {
+    print.noquote("*** Entry must be INTEGER ***")
+    inp <- oral_diet_idx()
+  }
+  return(inp)
+}
+
+#get clinical condition (if oral diet not regular)
 clinical_condition_idx <- function()
 {
   print.noquote("What is the indication for your diet order?")
@@ -93,36 +89,149 @@ clinical_condition_idx <- function()
     print.noquote(paste(i, clinical_condition_options[i], sep = " - "))
   }
   inp <- readline(prompt="Enter clinical condition as integer: ")
-  return(as.integer(inp))
+  inp <- as.integer(inp)
+  if (is.na(inp)) {
+    print.noquote("*** Entry must be INTEGER ***")
+    inp <- clinical_condition_idx()
+  }
+  return(inp)
 }
 
-#get NPO exceptions
+#get NPO exceptions (if oral diet == npo with exceptions)
 npo_exceptions_idx <- function()
 {
-  print.noquote("What items can your patient swallow by mouth?")
+  print.noquote("Patient NPO with exceptions. What can the patient be given by mouth?")
   print.noquote("Oral diet 'nothing with exceptions' options:")
-  for (i in 1:length(oral_diet_exceptions)) {
-    print.noquote(paste(i, oral_diet_exceptions[i], sep = " - "))
+  for (i in 1:length(npo_exceptions_options)) {
+    print.noquote(paste(i, npo_exceptions_options[i], sep = " - "))
   }
   inp <- NA
-  while (sum(grepl("done", inp)) < 1) {
-    inp <- append(inp, readline(prompt="Enter NPO exceptions as integer (type done when finished): "))
+  while (sum(grepl(0, inp)) < 1) {
+    new <- readline(prompt="Enter NPO exceptions as integer (type 0 when done): ")
+    new <- as.integer(new)
+    if (is.na(new)) {
+      print.noquote("*** Entry must be INTEGER ***")
+      new <- npo_exceptions_idx()
+    }
+    inp <- append(inp, new)
   }
   inp <- inp[!is.na(inp)]
   inp <- inp[c(1:length(inp)-1)]
   return(as.vector(inp))
 }
 
-#get proximal limit of enteral access
+#test for enteral ldas
+enteral_ldas_idx <- function()
+{
+  active_ldas <- possible_ldas[as.integer(active_ldas_idx())]
+  eli <- NULL
+  for (i in 1:length(active_ldas)) {
+    eli <- append(eli, which(enteral_ldas == active_ldas[i]))   #which for exact match
+  }
+  return(eli)
+}
+
+#get enteral diet preference (if patient has enteral lda)
+enteral_diet_idx <- function()
+{
+  print.noquote("What can the patient be given by tube?")
+  print.noquote("Enteral diet order options:")
+  for (i in 1:length(enteral_diet_options)) {
+    print.noquote(paste(i, enteral_diet_options[i], sep = " - "))
+  }
+  inp <- NA
+  while (sum(grepl(0, inp)) < 1) {
+    new <- readline(prompt="Enter enteral diet order as integer (type 0 when done): ")
+    new <- as.integer(new)
+    if (is.na(new)) {
+      print.noquote("*** Entry must be INTEGER ***")
+      new <- enteral_diet_idx()
+    }
+    inp <- append(inp, new)
+  }
+  inp <- inp[!is.na(inp)]
+  inp <- inp[c(1:length(inp)-1)]
+  return(as.vector(inp))
+}
+
+#get proximal limit of enteral access (if patient has enteral lda) {eventually this should be limited by enteral LDAs}
 enteral_access_idx <- function()
 {
-  print.noquote("How far proximally can the patient's enteral tract be used?")
-  print.noquote("Enteral access options:")
-  for (i in 1:length(enteral_access_options)) {
-    print.noquote(paste(i, enteral_access_options[i], sep = " - "))
+  jlda <- grep("J", enteral_ldas[eldas])
+  glda <- NULL
+  for (i in 1:length(gastric_ldas)) {
+    glda <- append(glda, grep(gastric_ldas[i], enteral_ldas[eldas]))    #grep to catch PEG and PEG-J
   }
-  inp <- readline(prompt="Enter proximal limit of enteral access as integer: ")
-  return(as.integer(inp))
+  if ( length(glda) > 0 ) {
+    if ( length(jlda) > 0 ) {
+      print.noquote("Patient has tubes at multiple enteric levels (e.g. gastric, jejunal).")
+      print.noquote("How far proximally can the patient's enteral tract be used?")
+      print.noquote("Enteral access options:")
+      for (i in 1:length(enteral_access_options)) {
+        print.noquote(paste(i, enteral_access_options[i], sep = " - "))
+      }
+      inp <- readline(prompt="Enter proximal limit of enteral access as integer: ")
+      inp <- as.integer(inp)
+      if (is.na(inp)) {
+        print.noquote("*** Entry must be INTEGER ***")
+        inp <- enteral_access_idx()
+      }
+    } else {
+      print.noquote("Patient has a gastric tube.")
+      inp <- 1
+    }
+  } else if ( length(jlda) >0 ) {
+    print.noquote("Patient has a jejunal tube.")
+    inp <- 3
+  } else {
+    inp <- NULL
+  }
+  inp <- inp[!is.na(inp)]
+  return(inp)
+}
+
+#get aggregate diet order
+sum_diet <- function()
+{
+  sum_diet <- append("Oral diet:", oral_diet_options[odi])
+  if (!is.null(cci)) {
+    cc <- append("Clinical condition:", clinical_condition_options[cci])
+    if (!is.null(nei)) {
+      ne <- append("NPO exceptions:", npo_exceptions_options[nei])
+      if (!is.null(edi)) {
+        ed <- append("Enteral diet:", enteral_diet_options[edi])
+        if ( length(eai) > 0 ) {
+          ea <- append("Enteral access:", enteral_access_options[eai])
+          sum_diet <- append(sum_diet, c(cc, ne, ed, ea))
+        } else {
+          sum_diet <- append(sum_diet, c(cc, ne, ed))
+        }
+      } else {
+        sum_diet <- append(sum_diet, c(cc, ne))
+      }
+    } else if (!is.null(edi)) {
+      ed <- append("Enteral diet:", enteral_diet_options[edi])
+      if ( length(eai) >0 ) {
+        ea <- append("Enteral access:", enteral_access_options[eai])
+        sum_diet <- append(sum_diet, c(cc, ed, ea))
+      } else {
+        sum_diet <- append(sum_diet, c(cc, ed))
+      }
+    } else {
+      sum_diet <- append(sum_diet, cc)
+    }
+  } else if (!is.null(edi)) {
+    ed <- append("Enteral diet:", enteral_diet_options[edi])
+    if ( length(eai) > 0 ) {
+      ea <- append("Enteral access:", enteral_access_options[eai])
+      sum_diet <- append(sum_diet, c(ed, ea))
+    } else {
+      sum_diet <- append(sum_diet, ed)
+    }
+  } else {
+    sum_diet <- sum_diet
+  }
+  return(sum_diet)
 }
 
 
@@ -130,32 +239,13 @@ enteral_access_idx <- function()
 
 odi <- oral_diet_idx()
 
-
-### second line diet order to address enteral utilization preference
-
-## test for presence of enteral access; offer enteral diet options
-## only if patient has enteral access
-
-active_ldas <- possible_ldas[as.integer(active_ldas_idx())]
-ldas <- NULL
-for (i in 1:length(active_ldas)) {
-  ldas <- append(ldas, sum(grepl(active_ldas[i], enteral_ldas)))
-}
-if (sum(ldas) >= 1) {
-
-  #patient has active enteral access recorded
-  edi <- enteral_diet_idx()
-
-} else {
-
-  #patient does not have active enteral access recorded
-  print.noquote("Patient does not have active enteral access recorded.")
-  print.noquote("There are no enteral diet order preferences.")
-
+if (is.null(odi) | odi == 0) {
+  print.noquote("The patient must have an oral diet order. Please enter oral diet order as integer.")
+  odi <- oral_diet_idx()
 }
 
 
-### third line diet order to assess clinical condition
+### second line diet order to assess clinical condition
 
 ## test oral diet order; require clinical condition only if
 ## oral diet is != "regular"
@@ -176,13 +266,13 @@ if (odi != 1) {
 ## most patients made NPO for surgery or procedure can have exceptions
 
 if ( !is.null(cci) ) {
-    if (cci == 1 & odi != 4 ) {
-      print.noquote("Most patients awaiting surgery should have an oral diet order of nothing with exceptions, with the exceptions being medications and sips with medications.")
-    }
+  if (cci == 1 & odi != 4 ) {
+    print.noquote("Most patients awaiting surgery should have an oral diet order of nothing with exceptions, with the exceptions being medications and sips with medications.")
+  }
 }
 
 
-### fourth line diet order to address exceptions in the case of a "nothing w/ exceptions" value in oral diet order
+### third line diet order to address exceptions in the case of a "nothing w/ exceptions" value in oral diet order
 
 ## test oral diet order; offer exceptions options only if
 ## oral diet is "nothing with exceptions"
@@ -195,7 +285,28 @@ if (odi == 4) {
 } else {
 
   #patient does not have nothing with exceptions oral diet order
-  print.noquote("Patient is strictly NPO, without exception.")
+  print.noquote("No need to choose NPO exceptions.")
+
+}
+
+
+### fourth line diet order to address enteral utilization preference
+
+## test for presence of enteral access; offer enteral diet options
+## only if patient has enteral access
+
+eldas <- enteral_ldas_idx()
+
+if (length(eldas) > 0) {
+
+  #patient has active enteral access recorded
+  edi <- enteral_diet_idx()
+
+} else {
+
+  #patient does not have active enteral access recorded
+  print.noquote("Patient does not have active enteral access recorded.")
+  print.noquote("There are no enteral diet order preferences.")
 
 }
 
@@ -210,7 +321,7 @@ if ( !is.null(edi) ) {
   if ( sum( grepl(4, edi) ) > 0 ) {
 
     #patient has enteral diet set to "nothing at all"
-    print.noquote("The patient's enteric tract is not safe to use.")
+    print.noquote("The patient's tube should not be used to administer meds/feeds.")
 
   } else {
 
@@ -222,10 +333,8 @@ if ( !is.null(edi) ) {
 }
 
 
-### report diet order
+### report aggregate diet order
 
-#aggregate options and show final diet order
-
-#if (!is.null(odi)) {
-
-#}
+sum_diet <- sum_diet()
+print.noquote("Final diet order:")
+print.noquote(sum_diet)
